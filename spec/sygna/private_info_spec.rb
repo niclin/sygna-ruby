2dd2b0b65b351d1bcc28666b0c15f65c20c31c46
Nic <niclin0226@gmail.com>
2020-04-16 17:12:51 +0800

    Implement encode test

---
+++ spec/sygna/private_info_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+require "openssl"
+
+RSpec.describe Sygna::PrivateInfo do
+  before(:all) do
+    @setting = YAML.load(File.read(File.join('spec', 'fixtures', "settings.yml")))
+
+    Sygna.configure do |config|
+      config.private_key = @setting["private_key"]
+    end
+  end
+
+  describe ".encode" do
+    it "encode sensitive data" do
+      hash_data = { "name" => "Nic" }
+      public_key_hex = OpenSSL::PKey::EC.new(@setting["public_key"]).public_key.to_bn.to_s(16)
+
+      encoded_private_info = described_class.encode(hash_data, public_key_hex)
+      decoded_private_info = described_class.decode(encoded_private_info)
+
+      expect(encoded_private_info).to be_kind_of(String)
+      expect(JSON.parse(decoded_private_info)).to eq(hash_data)
+    end
+  end
+end
